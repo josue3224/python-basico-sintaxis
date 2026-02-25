846ea5ddd064b0ee02b8e07121cadb6bca74180e
josue3224 <1716843@senati.pe>
2026-02-25 12:25:01 -0500

    first commit

---
+++ 1-tipos-de-datos.py
@@ -0,0 +1,13 @@
+#tipos de datos en python
+
+#1. tipo de dato numerico
+edad = 23
+promedio = 19
+numero = -50
+#2. tipo de dato decimal (float)
+#3. tipo de dato texto (str)
+letra = 'A'
+nombre ='diego'
+#4. tipo de dato booleados (bool)
+es_mayor_de_edad = True 
+puede_volar=False
---
+++ 2-operadores.py
@@ -0,0 +1,35 @@
+#Operadores Aritmeticos
+
+#1. Operadores aritmeticos
+suma = 5 + 5 
+resta = 10 - 6
+multiplicacion = 5 * 5 
+division = 10 / 6
+modulo = 10 % 2
+exponente = 10 ** 2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print(exponente)
+
+#operadores de comparacion
+print( 5 == 5 )   #igual 
+print( 5 != 5 )   #diferente de 
+print( 10 > 5 )   #mayor que
+print( 10  < 5 )
+print( 10 <= 5 )
+print( 10 >= 5 )
+#3 operadores logicos 
+v = True 
+f = False
+#3.1. and (y)
+print(v and v)  
+print(v and f) 
+print(f and v)
+print(f and f)
+
+#3.3. not (negacion)
+print(not v )
+print(not f)
